cb8d455cb748836664850075275272ce3903c243
AmitParihar17 <amitparihar1208@gmail.com>
2026-02-08 21:50:42 +0530

    add new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
+import FeaturedCourses from "../components/FeaturedCourses";
 import HeroSection from "../components/HeroSection";
+import WhyChooseUs from "../components/WhyChooseUs";
 
  
  const page = () => {
    return (
      <div>
        <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-         <h1 className="text-2xl text-center">Home</h1>
          <HeroSection />
+         <FeaturedCourses />
+         <WhyChooseUs />
        </main>
      </div>
    );
